05683f488d5d135ea1f4c871624409b5a52f1101
Yoshimura Yuu <yyu@mental.poker>
2025-02-08 16:25:49 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-format.git", from: "600.0.0"),
         .package(url: "https://github.com/yonaskolb/XcodeGen.git", from: "2.42.0"),
-        .package(url: "https://github.com/mono0926/LicensePlist.git", from: "3.25.1"),
+        .package(url: "https://github.com/mono0926/LicensePlist.git", from: "3.26.0"),
     ]
 )
