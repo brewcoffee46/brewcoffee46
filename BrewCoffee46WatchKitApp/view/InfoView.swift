eeba00774981fa734ff862bbddf4cccc56c9018f
Yoshimura Yuu <yyu@mental.poker>
2025-02-10 21:39:12 +0900

    Show error log in WatchKit App

--- BrewCoffee46WatchKitApp/view/InfoView.swift
+++ BrewCoffee46WatchKitApp/view/InfoView.swift
@@ -1,10 +1,18 @@
 import SwiftUI
 
 struct InfoView: View {
+    @EnvironmentObject var viewModel: CurrentConfigViewModel
+
     var body: some View {
         Form {
             Section(header: Text("info version")) {
                 Text((Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String)!)
+            }
+
+            if !viewModel.log.isEmpty {
+                Section(header: Text("watch kit app info log header")) {
+                    Text(viewModel.log)
+                }
             }
         }
     }
