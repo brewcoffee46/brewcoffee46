825da6ddcc0b16faa1903285e4484de15a5fed83
Yoshimura Yuu <yyu@mental.poker>
2025-07-20 13:59:29 +0900

    Add Crashlytics to watchOS app

--- BrewCoffee46WatchKitApp/WatchApp.swift
+++ BrewCoffee46WatchKitApp/WatchApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct WatchApp: App {
+    @WKApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             RootView()
